ff369ffba2e3ab5ed358f259046eee9e113d6ffd
Razvan Chelemen <chelemen.razvan@gmail.com>
2019-03-05 17:12:46 +0200

    Demo project

---
+++ Example/AwesomeProject/App.js
@@ -0,0 +1,56 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow
+ * @lint-ignore-every XPLATJSCOPYRIGHT1
+ */
+
+import React, {Component} from 'react';
+import {Platform, StyleSheet, Text, View} from 'react-native';
+import {NativeModules} from 'react-native';
+import { requireNativeComponent } from 'react-native';
+import { TouchableOpacity } from 'react-native';
+
+var RNDeliverer = NativeModules.RNDeliverer;
+RNDeliverer.addEvent('Birthday Party', '4 Privet Drive, Surrey');
+RNDeliverer.setupStreamer()
+RNDeliverer.startCamera()
+RNDeliverer.addStreamingEndpoint('rtmp://192.168.0.64:1935/live/test')
+RNDeliverer.startEncoding()
+
+const RNCameraView = requireNativeComponent("RNCameraView")
+
+const instructions = Platform.select({
+  ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
+  android:
+    'Double tap R on your keyboard to reload,\n' +
+    'Shake or press menu button for dev menu',
+});
+
+type Props = {};
+export default class App extends Component<Props> {
+  render() {
+    return (
+      <View style={styles.container}>
+        <RNCameraView style={ styles.wrapper } />
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1, alignItems: "stretch"
+  },
+  wrapper: {
+    flex: 1, alignItems: "center", justifyContent: "center"
+  },
+  border: {
+    borderColor: "#eee", borderBottomWidth: 1
+  },
+  button: {
+    fontSize: 50, color: "orange"
+  }
+});
---
+++ Example/AwesomeProject/ios/DummySwift.swift
@@ -0,0 +1,13 @@
+//
+//  DummySwift.swift
+//  AwesomeProject
+//
+//  Created by Razvan Chelemen on 05/03/2019.
+//  Copyright © 2019 Facebook. All rights reserved.
+//
+
+import UIKit
+
+class DummySwift: NSObject {
+
+}
